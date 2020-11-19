978e97f8e74954cda2bfef18ac5b05dd6b80892c
Joseph Hejderup <jhejderup@users.noreply.github.com>
2020-11-19 17:07:25 +0100

    Refactor projects layout

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,10 @@ const markdownIt = require("markdown-it");
 const markdownItEmoji = require("markdown-it-emoji");
 
 module.exports = function(eleventyConfig) {
-  const options = {
-    html: true
-  };
-  const markdownLib = markdownIt(options).use(markdownItEmoji);
+
+    let markdownLib = markdownIt({html: true}).use(markdownItEmoji);
   
-  eleventyConfig.setLibrary("md", markdownLib);
+    eleventyConfig.setLibrary("md", markdownLib);
 
 
     // {{ variable | jsonify }}
@@ -18,21 +16,14 @@ module.exports = function(eleventyConfig) {
       return JSON.stringify(variable);
     });
 
-    eleventyConfig.setLiquidOptions({
-      dynamicPartials: false,
-      root: [
-        '_includes',
-        '.'
-      ]
-    });
 
     eleventyConfig.addCollection('news', collection => {
       return collection.getFilteredByGlob('_news/*.md');
     });
     
     eleventyConfig.addCollection('projects', collection => {
-      return collection.getFilteredByGlob('_projects/*.markdown')
-          .sort((a, b) => b.data.importance - a.data.importance);
+      return collection.getFilteredByGlob('_projects/*.md')
+          .sort((a, b) => a.data.importance - b.data.importance);
     });
 
     eleventyConfig.addCollection('posts', collection => {
@@ -57,6 +48,7 @@ module.exports = function(eleventyConfig) {
 
     return {
       dir: {
+        includes: "_includes",
         layouts: "_layouts",
         input: "./",
         output: "./_site"
