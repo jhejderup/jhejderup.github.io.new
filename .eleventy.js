1ffe663d4efd4778d86f0145e15bd7847d4ff3fd
Joseph Hejderup <jhejderup@users.noreply.github.com>
2020-11-19 15:46:05 +0100

    Fix issues in the about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const embedEverything = require("eleventy-plugin-embed-everything");
+const pluginDate = require("eleventy-plugin-date");
+const markdownIt = require("markdown-it");
+const markdownItEmoji = require("markdown-it-emoji");
 
 module.exports = function(eleventyConfig) {
+  const options = {
+    html: true
+  };
+  const markdownLib = markdownIt(options).use(markdownItEmoji);
+  
+  eleventyConfig.setLibrary("md", markdownLib);
 
 
     // {{ variable | jsonify }}
@@ -30,6 +39,10 @@ module.exports = function(eleventyConfig) {
       return collection.getFilteredByGlob('_posts/*.md');
     });
 
+    eleventyConfig.addCollection('pages', collection => {
+      return collection.getFilteredByGlob('_pages/*.md');
+    });    
+
     // Copy the `assets` directory to the compiled site folder
     eleventyConfig.addPassthroughCopy('assets');
 
@@ -38,6 +51,9 @@ module.exports = function(eleventyConfig) {
 
     // Enable embeddings of stuff
     eleventyConfig.addPlugin(embedEverything);
+    
+    //Date formats
+    eleventyConfig.addPlugin(pluginDate);
 
     return {
       dir: {
